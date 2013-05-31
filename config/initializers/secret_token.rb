fb788e576f2909e41ce587e24a4e40a433703395
Mike O'Dell <mike@gift-give.com>
2013-05-30 23:31:05 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheRealDeal::Application.config.secret_token = 'fcbbe3530bee5981a63010bc30b784cfa69b1b4845e72dd5231a0dfe07074fadd3902018eead54898c443ce7773b543a78b497f8a94d8ec2e873206912017eb7'
